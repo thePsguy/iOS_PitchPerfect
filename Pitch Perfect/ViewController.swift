d835764f2bfd62a7da259fa97dba4ef95340bddb
Dustin Howell <dkbhowell@gmail.com>
2017-01-27 14:56:51 -0800

    Initial commit with UI mostly laid out

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    // MARK: Actions
+    @IBAction func recordAudio(_ sender: UIButton) {
+        recordLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
 
+    @IBAction func stopRecording(_ sender: UIButton) {
+        recordLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        stopRecordingButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
